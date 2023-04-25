6379e47fbbcf79cea6c76b393f357e87e7ed928b
Vit Pavlik <pavlikvitek13@gmail.com>
2023-04-25 20:56:57 +0200

    redo the macros

--- makra.h
+++ makra.h
@@ -17,24 +17,33 @@
 #ifndef __MAKRA_H__
 #define __MAKRA_H__
 
-#include <stdio.h>
-
-#ifndef NDEBUG
-
 /**
  * Poznamka:
  * Zamerne nepouzivam usleep, protoze POSIX pravi ze je deprecated.
 */
-
-/* deklarace nanosleep je v `time.h` z nejakeho duvodu podminena definici 
-   tohoto symbolu */
-#ifndef __USE_POSIX199309
-#define __USE_POSIX199309
-#endif  // ifndef __USE_POSIX199309
+#include <stdio.h>         // fprintf
+#define __USE_POSIX199309  // nanosleep
 #include <time.h>          // nanosleep
-#include <limits.h>
-#include <assert.h>
-#include <stdlib.h>
+#include <stdlib.h>        // rand
+
+/* a bilion (for use in sleep_s macro definition) */
+#define BLN 1000000000
+
+/* pauses the execution of the program for `t` seconds (`t` can be float) */
+#define sleep_s(t) \
+   nanosleep(&(struct timespec){ \
+                                .tv_sec  = ((long)((float)BLN * (t))) / BLN, \
+                                .tv_nsec = ((long)((float)BLN * (t))) % BLN \
+                               }, NULL)
+
+/* sleeps `t` milliseconds */
+#define sleep_ms(t) sleep_s(0.001 * t)
+
+/* sleeps for a random time between `a` and `b` seconds */
+#define sleep_rand(a, b) \
+   db_sleep((float)(a) + ((float)(rand()) / RAND_MAX) * ((float)(b) - (a)))
+
+#ifndef NDEBUG
 
 /* logs plain string or does nothing if NDEBUG is defined */
 #define log(msg) fprintf(stderr, __FILE__ ":%03d: " msg "\n", __LINE__)
@@ -43,20 +52,13 @@
 #define logv(msg, ...) fprintf(stderr, __FILE__ ":%03d: " msg "\n", \
                                __LINE__, __VA_ARGS__)
 
-/* a bilion (for use in db_sleep macro definition) */
-#define BLN 1000000000
 
 /* sleeps `t` (float) seconds or does nothing if NDEBUG is defined */
-#define db_sleep(t) \
-   nanosleep(&(struct timespec){ \
-                                .tv_sec  = ((long)((float)BLN * (t))) / BLN, \
-                                .tv_nsec = ((long)((float)BLN * (t))) % BLN \
-                               }, NULL)
+#define db_sleep(t) sleep_s(t)
 
 /* sleeps for a random time between `a` and `b` seconds
    or does nothing if NDEBUG is defined */
-#define db_sleep_rand(a, b) \
-   db_sleep((float)(a) + ((float)(rand()) / RAND_MAX) * ((float)(b) - (a)))
+#define db_sleep_rand(a, b) sleep_rand(a, b)
 
 #else  // ifndef NDEBUG
 
@@ -66,13 +68,4 @@
 #define db_sleep_rand(a, b) {}
 
 #endif  // ifndef NDEBUG
-
-
-/* print error */
-#define perror(msg) fprintf(stderr, __FILE__ ":%03d: " msg "\n", __LINE__)
-
-/* print error with format (like printf) */
-#define perrorf(msg, ...) fprintf(stderr, __FILE__ ":%03d: " msg "\n", \
-                                  __LINE__, __VA_ARGS__)
-
 #endif  // ifndef __MAKRA_H__
