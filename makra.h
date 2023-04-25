3e7bba5bbb0eca151e5f036beebe1bc94a656fdb
Vit Pavlik <pavlikvitek13@gmail.com>
2023-04-25 23:20:28 +0200

    redo macros

--- makra.h
+++ makra.h
@@ -17,14 +17,21 @@
 #ifndef __MAKRA_H__
 #define __MAKRA_H__
 
+
+/* toto je potreba pro nanoslep, v GNU99 to je definovane implicitne */
+#ifndef __USE_POSIX199309
+#define __USE_POSIX199309 
+#endif  // ifndef __USE_POSIX199309
+
+
+#include <stdio.h>         // fprintf
+#include <time.h>          // nanosleep
+#include <stdlib.h>        // rand
+
 /**
  * Poznamka:
  * Zamerne nepouzivam usleep, protoze POSIX pravi ze je deprecated.
 */
-#include <stdio.h>         // fprintf
-#define __USE_POSIX199309  // nanosleep
-#include <time.h>          // nanosleep
-#include <stdlib.h>        // rand
 
 /* a bilion (for use in sleep_s macro definition) */
 #define BLN 1000000000
@@ -43,6 +50,10 @@
 #define sleep_rand(a, b) \
    db_sleep((float)(a) + ((float)(rand()) / RAND_MAX) * ((float)(b) - (a)))
 
+
+/* tato makra jsou jenom pro debugovani */
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 #ifndef NDEBUG
 
 /* logs plain string or does nothing if NDEBUG is defined */
@@ -51,7 +62,6 @@
 /* logs variable(s) or does nothing if NDEBUG is defined */
 #define logv(msg, ...) fprintf(stderr, __FILE__ ":%03d: " msg "\n", \
                                __LINE__, __VA_ARGS__)
-
 
 /* sleeps `t` (float) seconds or does nothing if NDEBUG is defined */
 #define db_sleep(t) sleep_s(t)
@@ -68,4 +78,6 @@
 #define db_sleep_rand(a, b) {}
 
 #endif  // ifndef NDEBUG
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 #endif  // ifndef __MAKRA_H__
